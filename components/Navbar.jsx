066d67cd67ae611c1dcf27cefb324ed6d63dc463
Olliver Hughes <ohughes1207@gmail.com>
2023-07-16 16:24:09 +0100

    updated hover colours

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,15 +4,15 @@ import { FaBars } from 'react-icons/fa';
 
 export default function Navbar() {
   return (
-    <nav className="bg-red-600 fixed top-0 w-full flex">
-    <div>
-      <button className='btn bg-red-600 text-gray-100 hover:text-red-600 hover:bg-gray-100 my-4 mx-4 shadow-2xl rounded-4xl hover:rounded-xl transition-all duration-300'>
-        <FaBars size={65}/>
-      </button>
-    </div>
+    <header className="bg-red-600 fixed top-0 w-full flex">
       <div>
-        <img src="./mareep.png" alt="Mareep"/> 
+        <button className='btn bg-red-700 text-gray-100 hover:text-red-600 hover:bg-gray-100 my-4 mx-4 shadow-2xl ease-in-out hover:rounded-3xl transition-all duration-300'>
+          <FaBars size={65} />
+        </button>
       </div>
-    </nav>
+      <div>
+        <img src="./mareep.png" alt="logo"/> 
+      </div>
+    </header>
   )
 }
