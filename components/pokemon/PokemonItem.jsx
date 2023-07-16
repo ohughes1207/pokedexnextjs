e63436e4c5d31cb0fcbb1636a721145f961c54ae
Olliver Hughes <ohughes1207@gmail.com>
2023-07-16 17:03:56 +0100

    added PokemonItem component and page lists pokemon

---
+++ components/pokemon/PokemonItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function PokemonItem({ pokemonData, variantData }) { 
+  return (
+        <div>
+          {pokemonData.map((base) => {
+            const variants = variantData.filter(
+              (variant) => variant.pokedex_num === base.pokedex_num
+            );
+  
+            return (
+              <div className='p-2 border rounded' key={base.id}>
+                <h1 className='text-2xl font-semibold'>{base.base_name}</h1>
+                <h2 className='text-xl font-semibold'>{base.pokedex_num}</h2>
+                {variants.length > 0 && (
+                <img className='h-20 w-20' key={variants[0].id} src={`/pokemon_imgs/${variants[0].img_name}`}/>
+              )}
+              </div>
+            );
+          })}
+        </div>
+    );
+}
--- pages/pokemon.js
+++ pages/pokemon.js
@@ -1,33 +1,33 @@
 import React from 'react'
+import PokemonItem from '@/components/pokemon/PokemonItem'
 import Head from 'next/head'
 
-export default function Pokemon({pokemonData}) {
-  return (
-    <>
-        <Head>
-            <title>Pokemon</title>
-            <meta name='description' content='Pokemon List'/>
-        </Head>
-        <div>
-            {pokemonData.map( p => (
-                <div className='p-2 border rounded'>
-                    <h1 className='text-2xl font-semibold'>{p.base_name}</h1>
-                    <h2 className='text-xl font-semibold'>{p.pokedex_num}</h2>
-                </div>
-            ) ) }
-        </div>
-    </>
-  )
+
+
+export default function Pokemon({ pokemonData, variantData }) {
+    return (
+        <>
+            <Head>
+                <title>Pokemon</title>
+                <meta name='description' content='Pokemon List' />
+            </Head>
+
+            <PokemonItem pokemonData={pokemonData} variantData={variantData}/>
+        </>
+    )
 }
 
 
 export async function getServerSideProps(context) {
-    const response = await fetch(`${process.env.NEXT_API_URL}/pokemon`)
-    const pokemonData = await response.json()
-    console.log("data", pokemonData)
+    const pokemonResponse = await fetch(`${process.env.NEXT_API_URL}/pokemon`)
+    const variantResponse = await fetch(`${process.env.NEXT_API_URL}/variants`)
+    const pokemonData = await pokemonResponse.json()
+    const variantData = await variantResponse.json()
+    console.log("data", variantData)
     return {
         props: {
-            pokemonData
+            pokemonData,
+            variantData
         }
     }
 }
