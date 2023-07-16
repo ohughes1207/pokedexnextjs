3c710721358e03e4c61f2a62977d9b89ffacabae
Olliver Hughes <ohughes1207@gmail.com>
2023-07-16 13:41:10 +0100

    stylized pokemon page

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Head from 'next/head'
-export default function Pokemon() {
+
+export default function Pokemon({pokemonData}) {
   return (
     <>
         <Head>
             <title>Pokemon</title>
             <meta name='description' content='Pokemon List'/>
         </Head>
+        <div>
+            {pokemonData.map( p => (
+                <div className='p-2 border rounded'>
+                    <h1 className='text-2xl font-semibold'>{p.base_name}</h1>
+                    <h2 className='text-xl font-semibold'>{p.pokedex_num}</h2>
+                </div>
+            ) ) }
+        </div>
     </>
   )
 }
@@ -14,11 +23,11 @@ export default function Pokemon() {
 
 export async function getServerSideProps(context) {
     const response = await fetch(`${process.env.NEXT_API_URL}/pokemon`)
-    const data = await response.json()
-    console.log("data", data)
+    const pokemonData = await response.json()
+    console.log("data", pokemonData)
     return {
         props: {
-            data
+            pokemonData
         }
     }
 }
