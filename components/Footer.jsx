53d30070a9190693332aa96f5f449dd970292601
Olliver Hughes <ohughes1207@gmail.com>
2023-07-16 16:23:47 +0100

    Footer showing now

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className=" bg-gray-800 text-white py-10 absolute bottom-0 w-full">
+      <div className="justify-center text-center rounded-xl bg-slate-400">
+        <img src='../img/mareep.png' alt='avatar'/>
+      </div>
     </footer>
-    )
+  );
 }
