7b5735ad8498f13e3f3d9e943da503e2bc6f2a77
Olliver Hughes <ohughes1207@gmail.com>
2023-07-16 18:39:06 +0100

    added logo with href

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,12 +6,14 @@ export default function Navbar() {
   return (
     <header className="bg-red-600 fixed top-0 w-full flex">
       <div>
-        <button className='btn bg-red-700 text-gray-100 hover:text-red-600 hover:bg-gray-100 my-4 mx-4 shadow-2xl ease-in-out hover:rounded-3xl transition-all duration-300'>
+        <button className='btn bg-red-600 text-gray-100 hover:text-red-600 hover:bg-gray-100 my-4 mx-4 shadow-2xl ease-in-out hover:rounded-3xl transition-all duration-300'>
           <FaBars size={65} />
         </button>
       </div>
-      <div>
-        <img src="./mareep.png" alt="logo"/> 
+      <div className="image-container" style={{ background: 'none' }}>
+        <a href='/'>
+          <img src="mareep-nobg.png" alt="logo" className='-my-24 -mx-40 scale-[0.35]'/> 
+        </a>
       </div>
     </header>
   )
