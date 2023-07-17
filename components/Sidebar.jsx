5ac7fed7c87b902d329c8a5780e7eb190a254d01
Olliver Hughes <ohughes1207@gmail.com>
2023-07-17 04:47:28 +0100

    Renamed tooltip on /pokemon sidebar button

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
   return (
     <div className='fixed left-0 w-32 flex flex-col bg-red-500 shadow-lg rounded-r-3xl top-1/3 py-3'>
       <a href='/pokemon'>
-        <SidebarIcon icon={<TbSearch size="85"/>} text='Search Pokemon' />
+        <SidebarIcon icon={<TbSearch size="85"/>} text='Pokedex' />
       </a>
       <a href='team-builder'>
         <SidebarIcon icon={<TbPokeball size="85"/>} text='Team Builder'/>
