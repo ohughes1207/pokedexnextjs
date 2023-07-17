fd4def47ae2d345a1228412fe2113f0c619ed527
Olliver Hughes <ohughes1207@gmail.com>
2023-07-17 04:25:27 +0100

    added rounded avatar component

--- components/index/Footer.jsx
+++ components/index/Footer.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className=" bg-gray-800 text-white py-10 absolute -bottom-[195px] w-full h-48">
-      <div className=" text-center rounded-xl bg-slate-400">
-        <img src='../img/mareep.png' alt='avatar'/>
-      </div>
+      <Avatar />
     </footer>
   );
 }
+
+
+const Avatar = () => (
+  <div className="w-36 h-36">
+    <img src='avatar.jpg' alt='avatar' className='rounded-full relative'/>
+  </div>
+)
