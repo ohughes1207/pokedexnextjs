6b1436b10030d2f6f74f5b6d33a765e674840398
Olliver Hughes <ohughes1207@gmail.com>
2023-07-17 04:26:31 +0100

    added sidebar to page

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PokemonItem from '@/components/pokemon/PokemonItem'
 import Head from 'next/head'
 import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+import AltBackground from '@/components/alt_bg'
 
 
 
@@ -13,6 +15,7 @@ export default function Pokemon({ pokemonData, variantData }) {
                 <meta name='description' content='Pokemon List' />
             </Head>
             <Navbar />
+            <Sidebar />
             <PokemonItem pokemonData={pokemonData} variantData={variantData}/>
         </>
     )
@@ -24,7 +27,6 @@ export async function getServerSideProps(context) {
     const variantResponse = await fetch(`${process.env.NEXT_API_URL}/variants`)
     const pokemonData = await pokemonResponse.json()
     const variantData = await variantResponse.json()
-    console.log("data", variantData)
     return {
         props: {
             pokemonData,
