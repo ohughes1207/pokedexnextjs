1d4eabe3d6d1826a1f05174208ae89b5c0025d49
Olliver Hughes <ohughes1207@gmail.com>
2023-07-17 01:19:16 +0100

    Sidebar on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Counter from '@/components/Counter'
@@ -8,14 +9,20 @@ const inter = Inter({ subsets: ['latin'] })
 import React from 'react'
 import Background from '@/components/index/background'
 import Footer from '@/components/index/Footer'
+import Sidebar from '@/components/Sidebar'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Pokedex App Homepage</title>
+        <meta name='description' content='Pokedex App Homepage' />
+      </Head>
       <Background />
       <Navbar />
+      <Sidebar />
       <Footer />
-    </>
+    </>  
   )
 }
 
