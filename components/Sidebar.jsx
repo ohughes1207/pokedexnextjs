a4238a0b52f911ae55bc81d78665859da1b7fabb
Olliver Hughes <ohughes1207@gmail.com>
2023-07-17 04:25:01 +0100

    increased size of sidebar icons

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,12 +5,12 @@ import { TbSearch } from 'react-icons/tb';
 
 export default function Sidebar() {
   return (
-    <div className='fixed left-0 w-32 flex flex-col bg-red-500 shadow-lg rounded-r-lg top-1/3'>
+    <div className='fixed left-0 w-32 flex flex-col bg-red-500 shadow-lg rounded-r-3xl top-1/3 py-3'>
       <a href='/pokemon'>
-        <SidebarIcon icon={<TbSearch size="75"/>} text='Search Pokemon' />
+        <SidebarIcon icon={<TbSearch size="85"/>} text='Search Pokemon' />
       </a>
       <a href='team-builder'>
-        <SidebarIcon icon={<TbPokeball size="75"/>} text='Team Builder'/>
+        <SidebarIcon icon={<TbPokeball size="85"/>} text='Team Builder'/>
       </a>
     </div>
   )
@@ -25,7 +25,3 @@ const SidebarIcon = ({ icon, text }) => (
       </span>
     </div>
 )
-
-/*
-absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md text-white bg-gray-900 text-xs font-bold transition-all duration-100 scale-0 origin-left
-*/
