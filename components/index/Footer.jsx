1c62ef2e23f603ff03ed2e767de76a62d1570a40
Olliver Hughes <ohughes1207@gmail.com>
2023-07-17 01:20:39 +0100

    Fine tuning position

--- components/index/Footer.jsx
+++ components/index/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className=" bg-gray-800 text-white py-10 absolute -bottom-[200px] w-full h-48">
+    <footer className=" bg-gray-800 text-white py-10 absolute -bottom-[195px] w-full h-48">
       <div className=" text-center rounded-xl bg-slate-400">
         <img src='../img/mareep.png' alt='avatar'/>
       </div>
