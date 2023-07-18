0b2601a892b5c7faafa46f87429835e7ef335ef8
Olliver Hughes <ohughes1207@gmail.com>
2023-07-18 19:32:52 +0100

    added case insensitive search and true equality operator

--- components/pokemon/hooks/SearchPokemon.js
+++ components/pokemon/hooks/SearchPokemon.js
@@ -1,3 +1,3 @@
 export default function SearchPokemon(pokemon, searchQuery) {
-  return searchQuery.toLowerCase() == '' ? pokemon : pokemon.base_name.toLowerCase().includes(searchQuery)
+  return searchQuery.toLowerCase() === '' ? pokemon : pokemon.base_name.toLowerCase().includes(searchQuery.toLowerCase())
 }
