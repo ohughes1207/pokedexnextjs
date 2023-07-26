01ade5ec2ad124a2f795c2d2647d525a89ad8c8f
Olliver Hughes <ohughes1207@gmail.com>
2023-07-26 12:05:15 +0100

    filter by generation working

--- components/pokedex/SearchMenu.jsx
+++ components/pokedex/SearchMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { usePokedex } from "./PokedexContext";
 import { AiOutlineCheck } from "react-icons/ai"
 
@@ -6,23 +6,26 @@ export default function SearchMenu () {
   const {setT1Filter, setT2Filter, setIsLegendary, isLegendary, isMythical, setIsMythical, isPseudoL, setIsPseudoL, isParadox, setIsParadox, isRegional, setIsRegional, isMega, setIsMega, isUB, setIsUB} = usePokedex()
 
   return (
-  <div className='h-96 border border-black mx-auto w-3/4 mt-32 flex p-5'>
-    <div className="border border-black m-5">
+  <div className='h-96 border border-black mx-auto w-3/4 mt-32 flex p-1 bg-red-500 rounded-3xl'>
+    <div className="border border-black m-3 shadow-lg mx-auto">
       <SearchBar />
-      <div className="border border-black w-2/3 flex ml-4 mt-16">
+      <div className="border border-black w-2/3 flex ml-4 shadow-lg">
         <TypeFilter TFilter={setT1Filter}/>
         <TypeFilter TFilter={setT2Filter}/>
       </div>
+      <div className="border border-black w-2/3 flex ml-4 shadow-lg">
+        <GenFilter />
+      </div>
     </div>
-    <div className="border border-black w-1/2 m-5">
-      <div className="border border-black h-1/2 flex">
+    <div className="border border-black w-1/2 m-3 mx-auto text-gray-100">
+      <div className="border border-black h-1/2 flex py-1">
         <Filter text={'Legendary'} filter={isLegendary} setFilter={setIsLegendary}/>
         <Filter text={'Mythical'} filter={isMythical} setFilter={setIsMythical}/>
         <Filter text={'Ultra Beast'} filter={isUB} setFilter={setIsUB}/>
         <Filter text={'Paradox'} filter={isParadox} setFilter={setIsParadox}/>
 
       </div>
-      <div className="border border-black h-1/2 flex">
+      <div className="border border-black h-1/2 flex py-1">
         <Filter text={'Regional'} filter={isRegional} setFilter={setIsRegional}/>
         <Filter text={'Mega'} filter={isMega} setFilter={setIsMega}/>
         <Filter text={'Pseudo-legendary'} filter={isPseudoL} setFilter={setIsPseudoL}/>
@@ -40,9 +43,9 @@ const Filter = ( {text, filter, setFilter } ) => {
   }; 
 
   return (
-    <div className="border border-black mx-auto">
-      <span className="text-2xl border border-black mx-3">{text}</span>
-      <button className={`flex items-center justify-center h-24 w-24 m-2 mx-auto text-gray-100 hover:text-red-500 hover:bg-gray-100 rounded hover:rounded-3xl transition-all duration-300 ease-linear cursor-pointer shadow-lg ${filter ? 'text-red-500 rounded-2xl bg-gray-100' : 'bg-red-500'}`} onClick={toggleFilter}>
+    <div className="border border-black mx-auto px-2">
+      <span className="text-3xl border border-black mx-auto flex justify-center px-1">{text}</span>
+      <button className={`flex items-center justify-center h-24 w-24 m-2 mx-auto text-gray-100 hover:text-red-500 hover:bg-gray-100 rounded hover:rounded-3xl transition-all duration-300 ease-linear cursor-pointer shadow-lg ${filter ? 'text-red-500 rounded-3xl bg-gray-100' : 'bg-red-500'}`} onClick={toggleFilter}>
         {<AiOutlineCheck size="85"/>}
       </button>
     </div>
@@ -55,7 +58,30 @@ const SearchBar = () => {
     setSearchQuery(e.target.value);
   };  
   return (
-    <input className='border rounded border-black w-2/3 text-5xl m-4' onChange={handleInputChange} type="text" placeholder="Search Pokemon"/>
+    <input className='border rounded border-black w-2/3 text-5xl m-4 shadow-lg' onChange={handleInputChange} type="text" placeholder="Search Pokemon"/>
+  );
+}
+
+
+const GenFilter = () => {
+  const { setGenValue } =usePokedex()
+  const handleInputChange = (e) => {
+    console.log(e.target.value)
+    setGenValue(e.target.value);
+  };
+  return (
+    <select className="border border-black text-4xl m-4 mx-auto" onChange={handleInputChange}>
+      <option value=""></option>
+      <option value="1">Generation 1</option>
+      <option value="2">Generation 2</option>
+      <option value="3">Generation 3</option>
+      <option value="4">Generation 4</option>
+      <option value="5">Generation 5</option>
+      <option value="6">Generation 6</option>
+      <option value="7">Generation 7</option>
+      <option value="8">Generation 8</option>
+      <option value="9">Generation 9</option>
+    </select>
   );
 }
 
@@ -65,7 +91,7 @@ const TypeFilter = ( {TFilter }) => {
     TFilter(e.target.value);
   };
   return (
-    <select className="border border-black text-4xl m-4" onChange={handleInputChange}>
+    <select className="border border-black text-4xl m-4 mx-auto" onChange={handleInputChange}>
       <option value=""></option>
       <option value="normal">Normal</option>
       <option value="fire">Fire</option>
--- components/pokedex/functions/FilterPokemon.js
+++ components/pokedex/functions/FilterPokemon.js
@@ -2,11 +2,16 @@ export default function FilterPokemon(pokemon, searchQuery, T1Filter, T2Filter, 
   
   let filteredPokemon = pokemon.filter(poke => poke.base_name.toLowerCase().includes(searchQuery.toLowerCase()));
 
-  if (T1Filter !== '') {
+  if (T1Filter) {
     filteredPokemon = filteredPokemon.filter(poke => poke.variants[0].type_1.toLowerCase().includes(T1Filter.toLowerCase()) || poke.variants[0].type_2?.toLowerCase().includes(T1Filter.toLowerCase()));
   }
-  if (T2Filter !== '') {
-    filteredPokemon = filteredPokemon.filter(poke => poke.variants[0].type_1.toLowerCase().includes(T2Filter.toLowerCase()) || poke.variants[0].type_2?.toLowerCase().includes(T2Filter.toLowerCase()));
+  if (T2Filter) {
+    if (T2Filter === T1Filter) {
+      filteredPokemon = filteredPokemon.filter(poke => (!poke.variants[0].type_2))
+    }
+    else {
+      filteredPokemon = filteredPokemon.filter(poke => poke.variants[0].type_1.toLowerCase().includes(T2Filter.toLowerCase()) || poke.variants[0].type_2?.toLowerCase().includes(T2Filter.toLowerCase()));
+    }
   }
   if (isLegendary) {
     filteredPokemon = filteredPokemon.filter(poke => poke.legendary == isLegendary);
@@ -29,32 +34,9 @@ export default function FilterPokemon(pokemon, searchQuery, T1Filter, T2Filter, 
   if (isMythical) {
     filteredPokemon = filteredPokemon.filter(poke => poke.mythical === isMythical);
   }
-  /*
-  if (genValue !== '') {
-    filteredPokemon = filteredPokemon.filter(poke => poke.gen === genValue);
+  if (genValue) {
+    filteredPokemon = filteredPokemon.filter(poke => poke.gen == genValue);
   }
-  if (isLegendary) {
-    filteredPokemon = filteredPokemon.filter(poke => poke.legendary === isLegendary);
-  }
-  if (isParadox) {
-    filteredPokemon = filteredPokemon.filter(poke => poke.paradox === isParadox);
-  }
-  if (isPseudoL) {
-    filteredPokemon = filteredPokemon.filter(poke => poke.pseudo_legendary === isPseudoL);
-  }
-  if (isUB) {
-    filteredPokemon = filteredPokemon.filter(poke => poke.UB === isUB);
-  }
-  if (isMythical) {
-    filteredPokemon = filteredPokemon.filter(poke => poke.mythical === isMythical);
-  }
-  if (isRegional) {
-    filteredPokemon = filteredPokemon.filter(poke => poke.variants && poke.variants.some(variant => variant.regional));
-  }
-  if (isMega) {
-    filteredPokemon = filteredPokemon.filter(poke => poke.variants && poke.variants.some(variant => variant.mega));
-  }
-  */
   return filteredPokemon;
 }
 
