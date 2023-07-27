b9d18a5d61bd8dc891ee4b394ac7df44436fef41
Olliver Hughes <ohughes1207@gmail.com>
2023-07-27 13:33:02 +0100

    changed icon in tab

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="/mareep-nobg.ico"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
