ec144d3bbf6a2a3e1b396a27792f1ba70a73cdb1
Olliver Hughes <ohughes1207@gmail.com>
2023-08-16 20:12:38 +0100

    moved -ml-32 into ternary operator

--- components/UI/Sidebar.jsx
+++ components/UI/Sidebar.jsx
@@ -6,7 +6,7 @@ import { useUI } from './UIContext';
 export default function Sidebar() {
   const { SidebarVisible } = useUI()
   return (
-    <div className={`fixed left-0 w-32 flex flex-col bg-red-500 shadow-lg rounded-r-3xl top-1/3 py-3 -ml-32 duration-300 ${SidebarVisible ? 'ml-0' : null}`}>
+    <div className={`fixed left-0 w-32 flex flex-col bg-red-500 shadow-lg rounded-r-3xl top-1/3 py-3 duration-300 transition-all ${SidebarVisible ? 'ml-0' : '-ml-32'}`}>
       <a href='/pokedex'>
         <SidebarIcon icon={<TbSearch size="85"/>} text='Pokedex' />
       </a>
