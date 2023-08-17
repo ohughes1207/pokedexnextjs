aba1696789904323314e175f3794510644a7bc1e
Olliver Hughes <ohughes1207@gmail.com>
2023-08-17 17:16:51 +0100

    changed server side props fetch

--- pages/team-builder.js
+++ pages/team-builder.js
@@ -25,20 +25,19 @@ export default function TeamBuilder() {
 }
 
 export async function getServerSideProps() {
-    const variantResponse = await fetch(`${process.env.NEXT_API_URL}/variants`)
-    const variantData = await variantResponse.json()
-    const typeResponse = await fetch(`${process.env.NEXT_API_URL}/types`)
-    const typeData = await typeResponse.json()
 
-    variantData.forEach((variant) => {
-        variant.type_1_rel = typeData.filter((variant) => typeData.type_name === variant.type_1);
-      });
-    variantData.forEach((variant) => {
-        variant.type_2_rel = typeData.filter((variant) => typeData.type_name === variant.type_2);
-      });    
+    const pokemonResponse = await fetch(`${process.env.NEXT_API_URL}/pokemon?page=1`)
+
+    const dataFetched = await pokemonResponse.json()
+    
+    const pokemonData = dataFetched.data
+
+
+    //console.log("pokemonData type:", typeof pokemonData);
+    //console.log(process.env.NEXT_API_URL)
     return {
         props: {
-            variantData
+            pokemonData,
         }
     }
 }
