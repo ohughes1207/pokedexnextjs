ab212686b836d5c45e76f099ec7eb7e4a1f04404
Olliver Hughes <ohughes1207@gmail.com>
2023-08-17 09:52:40 +0100

    fixed shadow on button rotating

--- components/pokedex/PokemonItem.jsx
+++ components/pokedex/PokemonItem.jsx
@@ -105,10 +105,10 @@ const PokemonCard = ( {pokemon} ) => {
           <UniqueAttributes pokemon={pokemon}/>
         </div>
         <button
-          className={`w-48 h-48 my-auto mx-auto rounded-full shadow-md flex justify-center items-center transition-all duration-500 ${variantsVisible ? `text-${GetTypeStyle(pokemon.variants[0].type_1)} bg-gray-100  rotate-[225deg]` : `bg-${GetTypeStyle(pokemon.variants[0].type_1)} text-gray-100 hover:bg-gray-100 hover:text-${GetTypeStyle(pokemon.variants[0].type_1)}`}`}
+          className={`w-48 h-48 my-auto mx-auto rounded-full shadow-md flex justify-center items-center transition-all duration-500 ${variantsVisible ? `text-${GetTypeStyle(pokemon.variants[0].type_1)} bg-gray-100 ` : `bg-${GetTypeStyle(pokemon.variants[0].type_1)} text-gray-100 hover:bg-gray-100 hover:text-${GetTypeStyle(pokemon.variants[0].type_1)}`}`}
           onClick={() => setVariantsVisible(!variantsVisible)}
           >
-          <PiPlusBold size={190}/>
+          <PiPlusBold size={190} className={`transition-all duration-500 ${variantsVisible ? 'rotate-[225deg]' : ''} `}/>
         </button>
       </div>
       <div className={`overflow-hidden relative bottom-3 grid transition-all duration-700 ${variantsVisible ? 'grid-rows-[minmax(0,1fr)]' : ' grid-rows-[minmax(0,0fr)]'}`}>
