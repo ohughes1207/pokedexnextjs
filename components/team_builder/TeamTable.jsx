f8c2e752020c145d7bfee2d4e6d18512973c6b52
Olliver Hughes <ohughes1207@gmail.com>
2023-08-18 08:57:51 +0100

    input field for searching

--- components/team_builder/TeamTable.jsx
+++ components/team_builder/TeamTable.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { useAmp } from 'next/amp'
+import React, { useState } from 'react'
 
 export default function TeamTable() {
   return (
@@ -8,32 +9,52 @@ export default function TeamTable() {
   )
 }
 
-const TableColumns = () => (
-    <>
-        <thead>
-            <tr>
-                <th>Move Type</th>
-                <th>Pokemon 1</th>
-                <th>Pokemon 2</th>
-                <th>Pokemon 3</th>
-                <th>Pokemon 4</th>
-                <th>Pokemon 5</th>
-                <th>Pokemon 6</th>
-                <th>Total Resistances</th>
-                <th>Total Weaknesses</th>
-                <th>Total Immunities</th>
-            </tr>
-        </thead>
-        <tbody>
-            {[
-            'Normal', 'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Fighting',
-            'Poison', 'Ground', 'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost',
-            'Dragon', 'Dark', 'Steel', 'Fairy'].map((typeName) => (
-            <TableRow key={typeName} typeName={typeName} className={`${typeName.toLowerCase()}Card`}/>
-            ))}
-        </tbody>
-    </>
-)
+
+const TableColumns = () => {
+    const [TM1, setTM1] = useState('');
+    const [TM2, setTM2] = useState('');
+    const [TM3, setTM3] = useState('');
+    const [TM4, setTM4] = useState('');
+    const [TM5, setTM5] = useState('');
+    const [TM6, setTM6] = useState('');
+
+
+    const handleInputChange = (e, func) => {
+        func(e.target.value);
+      };
+    
+    return (
+        <>
+            <thead>
+                <tr>
+                    <th>Move Type</th>
+                    <th className='w-32 mx-auto border border-black justify-center'>
+                        <input value={TM1} onChange={(e) => handleInputChange(e, setTM1)}>
+
+                        </input>
+                        <div></div>
+                    </th>
+                    <th className='border border-black'>Pokemon 2</th>
+                    <th>Pokemon 3</th>
+                    <th>Pokemon 4</th>
+                    <th>Pokemon 5</th>
+                    <th>Pokemon 6</th>
+                    <th>Total Resistances</th>
+                    <th>Total Weaknesses</th>
+                    <th>Total Immunities</th>
+                </tr>
+            </thead>
+            <tbody>
+                {[
+                'Normal', 'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Fighting',
+                'Poison', 'Ground', 'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost',
+                'Dragon', 'Dark', 'Steel', 'Fairy'].map((typeName) => (
+                <TableRow key={typeName} typeName={typeName} className={`${typeName.toLowerCase()}Card`}/>
+                ))}
+            </tbody>
+        </>
+    )
+}
 
 
 const TableRow = ({typeName}) => (
