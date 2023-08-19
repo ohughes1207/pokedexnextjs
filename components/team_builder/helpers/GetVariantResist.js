4003e771b1bced046f5051ffbf266602e364fa99
Olliver Hughes <ohughes1207@gmail.com>
2023-08-19 12:38:23 +0100

    Fixed get resist returning 0 for pure type pokemon

--- components/team_builder/helpers/GetVariantResist.js
+++ components/team_builder/helpers/GetVariantResist.js
@@ -2,6 +2,6 @@ export default function GetVariantResist(variant, moveType) {
     if (!variant || !variant.type_1_rel) {
       return null;
     }
-    return variant.type_1_rel[`def_vs_${moveType.charAt(0).toUpperCase() + moveType.slice(1)}`] * (variant.type_2_rel?.[`def_vs_${moveType.charAt(0).toUpperCase() + moveType.slice(1)}`] !== undefined ? variant.type_2_rel[`def_vs_${moveType.charAt(0).toUpperCase() + moveType.slice(1)}`] : null);
+    return variant.type_1_rel[`def_vs_${moveType.charAt(0).toUpperCase() + moveType.slice(1)}`] * (variant.type_2_rel?.[`def_vs_${moveType.charAt(0).toUpperCase() + moveType.slice(1)}`] !== undefined ? variant.type_2_rel[`def_vs_${moveType.charAt(0).toUpperCase() + moveType.slice(1)}`] : 1);
 }
   
