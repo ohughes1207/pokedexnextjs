a0ed223e0282446e20381df5e298b138445c1a99
Olliver Hughes <ohughes1207@gmail.com>
2023-08-20 13:07:26 +0100

    improved logic for helper functions

--- components/team_builder/helpers/GetTotalImmune.js
+++ components/team_builder/helpers/GetTotalImmune.js
@@ -1,13 +1,23 @@
 import GetVariantResist from "./GetVariantResist";
 
 export default function GetTotalImmune(team, moveType) {
-    let total = 0;
 
-    team.forEach((TM) => {
-        const value = GetVariantResist(TM, moveType);
-        if (value===0) {
-            total+=1;
-        }
-    })
-    return total
+    const hasObject = team.some(TM => typeof TM === 'object' && TM !== null);
+
+    if (hasObject) {
+        let total = 0;
+
+        team.forEach((TM) => {
+            if (typeof TM ==='object') {
+                const value = GetVariantResist(TM, moveType);
+                if (value===0) {
+                    total+=1;
+                }
+            }
+        });
+        return total;
+    }
+    else {
+        return null;
+    }
 }
