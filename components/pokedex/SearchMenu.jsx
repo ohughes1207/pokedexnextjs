700f281521003c6cb62ca63fa8a629645a4e4b14
Olliver Hughes <ohughes1207@gmail.com>
2023-08-27 17:19:46 +0100

    mobile search menu finished

--- components/pokedex/SearchMenu.jsx
+++ components/pokedex/SearchMenu.jsx
@@ -6,22 +6,22 @@ export default function SearchMenu () {
   const {setT1Filter, setT2Filter, setIsLegendary, isLegendary, isMythical, setIsMythical, isPseudoL, setIsPseudoL, isParadox, setIsParadox, isRegional, setIsRegional, isMega, setIsMega, isUB, setIsUB} = usePokedex()
 
   return (
-    <div className="bg-red-500 mt-32 w-min p-2 mx-auto xl:w-3/4">
+    <div className="bg-red-500 mt-32 w-min p-2 mx-auto xl:w-3/4 rounded-3xl">
       <div className="flex flex-col xl:flex-row">
-        <div className="rounded-3xl m-4 border border-black mx-auto">
-          <div className="m-2">
+        <div className="rounded-3xl mx-auto shadow-lg w-11/12 p-6">
+          <div className="flex justify-center mb-2">
             <SearchBar />
           </div>
-          <div className="w-full mx-auto flex justify-around px-5">
-            <TypeFilter />
-            <TypeFilter />
+          <div className="flex justify-around px-5 mb-2">
+            <TypeFilter TFilter={setT1Filter}/>
+            <TypeFilter TFilter={setT2Filter}/>
           </div>
-          <div className="flex mx-auto m-2">
+          <div className="flex justify-center">
             <GenFilter />
           </div>
         </div>
-        <div className="mx-auto rounded-3xl m-4 text-gray-100 flex-col border border-black p-2">
-          <div className="flex justify-evenly">
+        <div className="mx-auto m-4 text-gray-100 flex-col">
+          <div className="flex justify-evenly mb-2">
             <Filter text={'Legendary'} filter={isLegendary} setFilter={setIsLegendary}/>
             <Filter text={'Mythical'} filter={isMythical} setFilter={setIsMythical}/>
             <Filter text={'Ultra Beast'} filter={isUB} setFilter={setIsUB}/>
@@ -46,9 +46,9 @@ const Filter = ( {text, filter, setFilter } ) => {
   }; 
 
   return (
-    <div className="flex flex-col items-center justify-center m-2">
+    <div className="flex flex-col items-center justify-center mx-2">
       <span className="text-sm mx-auto text-center truncate">{text}</span>
-      <button className={` flex items-center justify-center w-16 h-16 mx-auto text-gray-100 hover:text-red-500 hover:bg-gray-100 rounded hover:rounded-3xl transition-all duration-300 ease-linear cursor-pointer shadow-lg ${filter ? 'text-red-500 rounded-3xl bg-gray-100' : 'bg-red-500'}`} onClick={toggleFilter}>
+      <button className={`flex items-center justify-center w-16 h-16 mx-auto text-gray-100 hover:text-red-500 hover:bg-gray-100 rounded hover:rounded-3xl transition-all duration-300 ease-linear cursor-pointer shadow-lg ${filter ? 'text-red-500 rounded-3xl bg-gray-100' : 'bg-red-500'}`} onClick={toggleFilter}>
         <AiOutlineCheck size="85"/>
       </button>
     </div>
@@ -73,7 +73,7 @@ const GenFilter = () => {
     setGenValue(e.target.value);
   };
   return (
-    <select className="mx-auto rounded-3xl text-center px-5 text-lg xl:text-4xl" onChange={handleInputChange}>
+    <select className="rounded-3xl text-center px-5 text-lg xl:text-4xl" onChange={handleInputChange}>
       <option value="0"></option>
       <option value="1">Generation 1</option>
       <option value="2">Generation 2</option>
