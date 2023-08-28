e1e2f60d5fd4eccaafcc19c02c478f27c0354451
Olliver Hughes <ohughes1207@gmail.com>
2023-08-28 13:37:47 +0100

    added z index so always on top

--- components/UI/Sidebar.jsx
+++ components/UI/Sidebar.jsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 export default function Sidebar() {
   const { SidebarVisible } = useUI()
   return (
-    <div className={`fixed left-0 w-32 flex flex-col bg-red-500 shadow-lg rounded-r-3xl top-1/3 py-3 duration-300 transition-all ${SidebarVisible ? 'ml-0' : '-ml-32'}`}>
+    <div className={`fixed left-0 w-32 flex flex-col bg-red-500 shadow-lg rounded-r-3xl top-1/3 py-3 duration-300 transition-all z-50 ${SidebarVisible ? 'ml-0' : '-ml-32'}`}>
       <Link href='/pokedex'>
         <SidebarIcon icon={<TbSearch size="85"/>} text='Pokedex' />
       </Link>
