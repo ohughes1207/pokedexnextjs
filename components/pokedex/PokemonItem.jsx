529aed356060c024324b7318d5fe548993b5ba3b
Olliver Hughes <ohughes1207@gmail.com>
2023-09-02 16:14:51 +0100

    progress on variant card for mobile

--- components/pokedex/PokemonItem.jsx
+++ components/pokedex/PokemonItem.jsx
@@ -34,9 +34,9 @@ export default function PokemonList ( { pokemonData, total_pages } ) {
 
 
   
-  if (filteredPokemonData.length == 0) {
+  if (filteredPokemonData.length === 0) {
     return (
-      <div className="mx-auto w-3/4 p-4 my-8 rounded-3xl bg-red-500">
+      <div className="mx-auto w-11/12 p-4 my-8 rounded-3xl bg-red-500">
         <p className=" text-center text-6xl text-gray-100 py-5">No Pokemon found!</p>
       </div>
     )
@@ -44,7 +44,7 @@ export default function PokemonList ( { pokemonData, total_pages } ) {
 
   return (
     <>
-      <div className="mx-auto w-5/6 p-2 my-8 rounded-3xl bg-red-500">
+      <div className="mx-auto w-11/12 p-2 my-8 rounded-3xl bg-red-500">
         {filteredPokemonData.map((pokemon) => (
           <PokemonCard pokemon={pokemon} key={pokemon.base_id}/>
           ))}
@@ -99,21 +99,21 @@ const PokemonCard = ( {pokemon} ) => {
   return (
     <>
       <div className={`mt-3 flex rounded-full bg-${GetTypeStyle(pokemon.variants[0].type_1)} shadow-xl relative z-10 p-2`}>
-        <img className='w-24 bg-black rounded-full shadow-lg' src={`/pokemon_imgs/${pokemon.variants[0].img_name}`} alt={pokemon.variants[0].img_name}/>
-        <div className="flex rounded-3xl shadow-xl border border-black ml-2 flex-col p-2 w-1/3">
+        <img className="w-24 sm:w-32 bg-black rounded-full shadow-lg" src={`/pokemon_imgs/${pokemon.variants[0].img_name}`} alt={pokemon.variants[0].img_name}/>
+        <div className="flex-grow ml-2 p-2 shadow-xl rounded-3xl sm:flex justify-between">
           <CommonAttributes pokemon={pokemon}/>
           <UniqueAttributes pokemon={pokemon}/>
         </div>
         <button
           className={`w-20 h-20 my-auto ml-2 mr-2 rounded-full shadow-md flex justify-center items-center transition-all duration-500 ${variantsVisible ? `text-${GetTypeStyle(pokemon.variants[0].type_1)} bg-gray-100 ` : `bg-${GetTypeStyle(pokemon.variants[0].type_1)} text-gray-100 hover:bg-gray-100 hover:text-${GetTypeStyle(pokemon.variants[0].type_1)}`}`}
           onClick={() => setVariantsVisible(!variantsVisible)}
-          >
-          <PiPlusBold size={190} className={`transition-transform duration-500 ${variantsVisible ? 'rotate-[225deg]' : undefined} `}/>
+        >
+          <PiPlusBold size={80} className={`transition-transform duration-500 ${variantsVisible ? 'rotate-[225deg]' : undefined} `}/>
         </button>
       </div>
       <div className={`relative bottom-3 grid transition-all duration-700 ${variantsVisible ? 'grid-rows-[minmax(0,1fr)]' : ' grid-rows-[minmax(0,0fr)]'}`}>
         <div className="overflow-hidden">
-          <div className={`mb-6 px-2 py-4 w-5/6 mx-auto rounded-b-3xl transition-all duration-700 bg-${GetTypeStyle(pokemon.variants[0].type_1)} ${variantsVisible ? 'translate-y-0' : ' -translate-y-full'} }`}>
+          <div className={`px-1 py-2 w-5/6 mx-auto rounded-b-3xl transition-all duration-700 bg-${GetTypeStyle(pokemon.variants[0].type_1)} ${variantsVisible ? 'translate-y-0' : ' -translate-y-full'} }`}>
             {pokemon.variants.map((variant) =>
               <VariantCard variant={variant} key={variant.var_id}/>
             )}
@@ -126,9 +126,9 @@ const PokemonCard = ( {pokemon} ) => {
 
 
 const VariantCard = ( {variant} ) => (
-  <div className={`h-1/3 mx-auto my-6 flex items-center rounded-full shadow-2xl bg-${GetTypeStyle(variant.type_1)}`} key={variant.var_id}>
-    <img className='w-16 m-4 bg-black rounded-full shadow-lg' src={`/pokemon_imgs/${variant.img_name}`} alt={variant.img_name}/>
-    <div className=" h-24 w-24 rounded-3xl flex p-2 shadow-lg">
+  <div className={`p-1 mx-auto mt-4 flex items-center rounded-full shadow-lg bg-${GetTypeStyle(variant.type_1)}`}>
+    <img className=' w-1/4 bg-black rounded-full shadow-lg' src={`/pokemon_imgs/${variant.img_name}`} alt={variant.img_name}/>
+    <div className="h-fit rounded-3xl flex p-1 shadow-lg flex-grow justify-between border">
       <VariantDetails variant={variant} />
       <VariantStats variant={variant} />
     </div>
@@ -137,8 +137,8 @@ const VariantCard = ( {variant} ) => (
 
 
 const VariantDetails = ( {variant} ) => (
-  <div className=" w-2/3 h-full ml-2">
-    <h1 className="text-xs">{variant.var_name}</h1>
+  <div className="h-full ml-2 mr-1 w-fit">
+    <h1 className="text-xs sm:text-base">{variant.var_name}</h1>
     <h2 className="text-xs py-2">{variant.type_1} {variant.type_2}</h2>
   </div>
 )
@@ -159,8 +159,8 @@ const VariantStats = ( {variant} ) => (
 
 const CommonAttributes = ( {pokemon} ) => (
   <div className="">
-    <h1 className='text-sm font-semibold'>{pokemon.pokedex_num}</h1>
-    <h1 className='text-sm font-semibold'>{pokemon.base_name}</h1>
+    <h1 className='text-sm sm:text-lg font-semibold'>{pokemon.pokedex_num}</h1>
+    <h1 className='text-sm sm:text-lg font-semibold'>{pokemon.base_name}</h1>
   </div>
 );
 
