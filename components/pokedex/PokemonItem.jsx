e6b8c887155daa76961af4c8dac9a393d8826ec0
Olliver Hughes <ohughes1207@gmail.com>
2023-09-17 00:34:57 +0100

    breakpoint size styling

--- components/pokedex/PokemonItem.jsx
+++ components/pokedex/PokemonItem.jsx
@@ -97,7 +97,7 @@ const PokemonCard = ( {pokemon} ) => {
 
   return (
     <>
-      <div className={`mt-3 2xl:mt-6 flex rounded-full bg-${GetTypeStyle(pokemon.variants[0].type_1)} shadow-xl relative z-10 pl-2 sm:p-2 md:p-3 2xl:p-4 h-28 sm:h-auto`}>
+      <div className={`mt-3 2xl:mt-6 flex rounded-full bg-${GetTypeStyle(pokemon.variants[0].type_1)} shadow-xl relative z-10 pl-2 sm:p-3 2xl:p-4 h-28 sm:h-auto`}>
         <img className="h-24 sm:h-32 md:h-40 lg:h-48 xl:h-56 2xl:h-72 2xl:m-4 my-auto bg-black rounded-full shadow-lg" src={`/pokemon_imgs/${pokemon.variants[0].img_name}`} alt={pokemon.variants[0].img_name}/>
         <div className="flex-grow ml-2 p-2 shadow-xl rounded-3xl flex flex-col justify-between 2xl:ml-10">
           <CommonAttributes pokemon={pokemon}/>
@@ -112,7 +112,7 @@ const PokemonCard = ( {pokemon} ) => {
       </div>
       <div className={`relative bottom-3 grid transition-all duration-700 ${variantsVisible ? 'grid-rows-[minmax(0,1fr)]' : ' grid-rows-[minmax(0,0fr)]'}`}>
         <div className="overflow-hidden">
-          <div className={`px-1 lg:px-2 py-2 lg:py-5 w-5/6 md:w-3/4 mx-auto rounded-b-3xl transition-all duration-700 bg-${GetTypeStyle(pokemon.variants[0].type_1)} ${variantsVisible ? 'translate-y-0' : ' -translate-y-full'} }`}>
+          <div className={`px-1 lg:px-2 xl:px-4 2xl:px-6 py-2 lg:py-5 w-5/6 md:w-3/4 2xl:w-4/5 mx-auto rounded-b-3xl transition-all duration-700 bg-${GetTypeStyle(pokemon.variants[0].type_1)} ${variantsVisible ? 'translate-y-0' : ' -translate-y-full'} }`}>
             {pokemon.variants.map((variant) =>
               <VariantCard variant={variant} key={variant.var_id}/>
             )}
@@ -125,9 +125,9 @@ const PokemonCard = ( {pokemon} ) => {
 
 
 const VariantCard = ( {variant} ) => (
-  <div className={`p-1 md:pl-2 mx-auto mt-4 flex items-center rounded-full shadow-lg bg-${GetTypeStyle(variant.type_1)} pr-8 xl:pr-20 border`}>
-    <img className='w-1/4 lg:w-[30%] xl:w-1/4 bg-black rounded-full shadow-lg mr-0.5 xl:mr-2' src={`/pokemon_imgs/${variant.img_name}`} alt={variant.img_name}/>
-    <div className="h-fit rounded-3xl flex p-0.5 md:pl-2 shadow-lg flex-grow justify-between pr-2 border">
+  <div className={`p-1 md:pl-1 lg:pl-0 md:py-0 md:h-1/3 mx-auto mt-4 flex items-center rounded-full shadow-lg bg-${GetTypeStyle(variant.type_1)} pr-8 sm:pr-11 lg:pr-14 xl:pr-24 border`}>
+    <img className='w-1/4 lg:w-[30%] xl:w-1/4 md:m-1 lg:m-2 xl:m-3 2xl:m-4 2xl:w-[21%] 2xl:w-1/5 2xl:w-56 bg-black rounded-full shadow-lg mr-0.5 sm:mr-1 xl:mr-2' src={`/pokemon_imgs/${variant.img_name}`} alt={variant.img_name}/>
+    <div className="h-fit rounded-3xl flex p-0.5 md:pl-1 xl:pl-2 shadow-lg flex-grow justify-between pr-2 border md:m-1">
       <VariantDetails variant={variant} />
       <VariantStats variant={variant} />
     </div>
@@ -136,7 +136,7 @@ const VariantCard = ( {variant} ) => (
 
 
 const VariantDetails = ( {variant} ) => (
-  <div className="h-full ml-0.5 mr-1 text-xs sm:text-base md:text-lg lg:text-xl xl:text-3xl">
+  <div className="h-full ml-2 mr-1 text-xs sm:text-base md:text-lg lg:text-xl xl:text-3xl 2xl:text-4xl">
     <h1 className="">{variant.var_name}</h1>
     <h2 className="py-2">{variant.type_1} {variant.type_2}</h2>
   </div>
@@ -144,7 +144,7 @@ const VariantDetails = ( {variant} ) => (
 
 
 const VariantStats = ( {variant} ) => (
-  <div className="text-xs w-20 xl:text-base xl:w-auto">
+  <div className="ml-1 text-xs w-20 lg:text-sm xl:text-base xl:w-auto 2xl:text-xl">
     <h1>Total: {variant.total_stats}</h1>
     <h2>HP: {variant.hp}</h2>
     <h2>Attack: {variant.att}</h2>
@@ -157,7 +157,7 @@ const VariantStats = ( {variant} ) => (
 
 
 const CommonAttributes = ( {pokemon} ) => (
-  <div className="text-sm sm:text-lg md:text-xl lg:text-3xl font-semibold w-fit">
+  <div className="text-sm sm:text-lg md:text-xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-semibold w-fit my-auto sm:m-1 lg:m-2 2xl:mx-4 ">
     <h1 className=''>{pokemon.pokedex_num}</h1>
     <h1 className=''>{pokemon.base_name}</h1>
   </div>
@@ -165,7 +165,7 @@ const CommonAttributes = ( {pokemon} ) => (
 
 
 const UniqueAttributes = ( {pokemon} ) => (
-  <div className="mt-auto text-sm font-semibold sm:text-lg md:text-xl lg:text-3xl">
+  <div className="mt-auto text-sm font-semibold sm:text-lg md:text-xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
     {pokemon.pseudo_legendary && (
       <h1 className=''>Pseudo-legendary</h1>
     )}
