600712660be6b5b9d1283a29206a898b47a3a67e
Olliver Hughes <ohughes1207@gmail.com>
2023-09-21 06:26:21 +0100

    Reset buttons fade in when a filter or TM is truthy

--- components/team_builder/ResetTableButton.jsx
+++ components/team_builder/ResetTableButton.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BiRevision } from 'react-icons/bi'
 import { useResetAtom } from "jotai/utils";
 import { TM1Atom, TM2Atom, TM3Atom, TM4Atom, TM5Atom, TM6Atom } from "./TeamTableAtoms";
+import { useAtomValue } from "jotai";
 
 
 
@@ -12,6 +13,15 @@ export default function ResetTableButton () {
     const resetTM4 = useResetAtom(TM4Atom);
     const resetTM5 = useResetAtom(TM5Atom);
     const resetTM6 = useResetAtom(TM6Atom);
+
+    const TM1 = useAtomValue(TM1Atom);
+    const TM2 = useAtomValue(TM2Atom);
+    const TM3 = useAtomValue(TM3Atom);
+    const TM4 = useAtomValue(TM4Atom);
+    const TM5 = useAtomValue(TM5Atom);
+    const TM6 = useAtomValue(TM6Atom);
+
+    const shouldShowButton = TM1 || TM2 || TM3 || TM4 || TM5 || TM6;
 
 
     const handleReset = () => {
@@ -27,7 +37,7 @@ export default function ResetTableButton () {
 
     return (
         <button 
-            className={`z-50 fixed top-2 right-4 sm:bottom-6 md:right-6 xl:bottom-6 xl:right-4 2xl:top-36 2xl:right-8 rounded-md w-24 h-24 mx-auto flex justify-center transition-all duration-500 items-center text-gray-100 bg-red-500 hover:bg-gray-100 hover:text-red-500 hover:rounded-3xl`}
+            className={`z-50 fixed top-2 right-4 sm:bottom-6 md:right-6 xl:bottom-6 xl:right-4 2xl:top-36 2xl:right-8 rounded-md w-24 h-24 mx-auto flex justify-center transition-all duration-500 items-center text-gray-100 bg-red-500 hover:bg-gray-100 hover:text-red-500 hover:rounded-3xl reset-button ${shouldShowButton ? 'visible' : 'invisible'}`}
             onClick={handleReset}
             >
             <BiRevision size={90}/> 
